bea21baad1feb6fe5bf6e382a075b11bbcf2f6bd
Kawakita-Yao <819043598@qq.com>
2025-07-26 21:14:03 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Yao's Blog
+> Astro + Fuwari theme + Netify Deploy
+
+## Frontmatter of Post
+
+```yaml
+---
+title: My First Blog Post
+published: 2025-07-15
+description: This is the first post of my new Astro blog.
+image: ./cover.jpg
+tags: [Foo, Bar]
+category: Front-end
+draft: false
+lang: zh-CN      # 仅当文章语言与 `config.ts` 中的网站语言不同时需要设置
+---
+```
---
+++ src/i18n/languages/zh_CN.ts
@@ -0,0 +1,38 @@
+import Key from "../i18nKey";
+import type { Translation } from "../translation";
+
+export const zh_CN: Translation = {
+	[Key.home]: "Home",
+	[Key.about]: "About",
+	[Key.archive]: "Archive",
+	[Key.search]: "Search",
+
+	[Key.tags]: "Tags",
+	[Key.categories]: "Categories",
+	[Key.recentPosts]: "RecentPosts",
+
+	[Key.comments]: "comments",
+
+	[Key.untitled]: "untitled",
+	[Key.uncategorized]: "uncategorized",
+	[Key.noTags]: "noTags",
+
+	[Key.wordCount]: "word",
+	[Key.wordsCount]: "words",
+	[Key.minuteCount]: "minute",
+	[Key.minutesCount]: "minutes",
+	[Key.postCount]: "post",
+	[Key.postsCount]: "posts",
+
+	[Key.themeColor]: "ThemeColor",
+
+	[Key.lightMode]: "Light",
+	[Key.darkMode]: "Dark",
+	[Key.systemMode]: "System",
+
+	[Key.more]: "More",
+
+	[Key.author]: "Author",
+	[Key.publishedAt]: "Published at",
+	[Key.license]: "License",
+};
